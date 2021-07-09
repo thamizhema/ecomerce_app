15546f90aa87e9810dcadb845054ba12fd76d13d
THAMIZH <thamizharasan2373@gmail.com>
2021-07-09 15:42:09 +0530

    product js aded

---
+++ src/screen/HomeScreen.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { products } from "../products";
+
+function HomeScreen() {
+  return (
+    <div>
+      <h1>product</h1>
+    </div>
+  );
+}
+
+export default HomeScreen;
